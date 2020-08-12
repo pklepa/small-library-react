c2dce71224f9acd8d402f31c689521d6efb8a3ac
klepa <pedro.klepa@gmail.com>
2020-08-12 17:18:32 -0300

    Updates

---
+++ precache-manifest.38f7ca890213dc50060182d3e1cb479d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8efd5a2ccbc81bc9e0985b468a209093",
+    "url": "/small-library-react/index.html"
+  },
+  {
+    "revision": "6730e41047c9a290c813",
+    "url": "/small-library-react/static/css/main.174798f2.chunk.css"
+  },
+  {
+    "revision": "d37e96942f7c39211496",
+    "url": "/small-library-react/static/js/2.30d8537c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/small-library-react/static/js/2.30d8537c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6730e41047c9a290c813",
+    "url": "/small-library-react/static/js/main.63677a88.chunk.js"
+  },
+  {
+    "revision": "59ea65dc24cfa4086d25",
+    "url": "/small-library-react/static/js/runtime-main.87c9ae7e.js"
+  },
+  {
+    "revision": "56cb1449656fb27a75229aff08e29b28",
+    "url": "/small-library-react/static/media/x.56cb1449.svg"
+  }
+]);
